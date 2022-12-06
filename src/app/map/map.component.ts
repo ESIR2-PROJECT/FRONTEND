9f34d81ee44baed01cb1f57fa14cfda329f27b03
Thomega35 <thomega35@gmail.com>
2022-12-06 18:16:59 +0100

    💄 UI Fix for the map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,22 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit{
   name:String = "Map";
-  
   ngOnInit(): void {
     var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
     mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWVnYSIsImEiOiJjbGJjZmVtcGgwM3FlM29xdTdqdTNzcGVoIn0.5H6TAGMFFAu-9maHaoW-BA';
     var map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      innerWidth: 1500,
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        innerWidth: 1500,
     });
-
   }
-
-
+  
 }
 
 
