f047a37499e1db61e9e2243c5d134b30e7b96302
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2022-12-13 10:34:48 +0100

    :fix: drawer toggling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MapComponent} from "./map/map.component";
+import {HomeComponent} from "./home/home.component";
 
 const routes: Routes = [
   { path: '',
     pathMatch: 'full',
     redirectTo: '/map',
   },
-  { path: 'map', component: MapComponent },
+  { path: 'map', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {SideNavService} from "./@core/side-nav/side-nav.service";
-import {MatDrawer} from "@angular/material/sidenav";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'FRONTEND';
-  @ViewChild('drawer', {static: true}) public drawer!: MatDrawer;
-  constructor(public sideNavService: SideNavService) { }
-
-  ngOnInit() {
-    this.sideNavService.setDrawer(this.drawer);
-  }
 }
