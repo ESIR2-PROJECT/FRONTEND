d29606c6e48ef60cba1a94ca23b1e791868a9edb
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2022-12-13 08:58:13 +0100

    :sparkles: nav bar
    :lipstick: angular materials
    
    :construction: side description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { SideDescriptionComponent } from './side-description/side-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    NavComponent,
+    SideDescriptionComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
