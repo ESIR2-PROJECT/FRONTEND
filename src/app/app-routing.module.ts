3b922c2967920ec738c00f5a5e0c47fa1f53f9d2
Tergel Tsagaan <tergel.tsagaan@gmail.com>
2022-12-13 09:26:14 +0100

    :sparkles: side nav spreading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MapComponent} from "./map/map.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',
+    pathMatch: 'full',
+    redirectTo: '/map',
+  },
+  { path: 'map', component: MapComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
