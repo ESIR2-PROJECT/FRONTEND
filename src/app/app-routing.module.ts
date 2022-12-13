96286335d2ddca15f1c259d67a29b58690c44ae1
Bastien Sauvat <bastien.sauvat@etudiant.univ-rennes1.fr>
2022-12-13 16:26:01 +0100

    timeslider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // other module options
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
-
+import { TimelineComponent } from './timeline/timeline.component';
+import { MglTimelineModule } from 'angular-mgl-timeline';
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    TimelineComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MglTimelineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
