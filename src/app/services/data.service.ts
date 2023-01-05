be8551582fb97df071b1e793c8dd025d4879df3d
fgdou <fabigoardou@gmail.com>
2023-01-05 09:32:57 +0100

    ✨ Add new data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Borne, Coordonnees, Horaire, Station, Ville } from '../objects/borne';
+import * as data from "./data.data"
 
 @Injectable({
   providedIn: 'root'
@@ -13,66 +14,21 @@ export class DataService {
   getAllBornes(): Borne[] {
     let bornes: Borne[] = []
 
-    // Borne 1
-    let nom_enseigne1 = "ELECTRA"
-    let nom_station1 = "Saint-Barthélémy d'Anjou"
-    let adresse_station1 = "8 rue de Champfleur 49124 Saint-Barthélemy-d'Anjou"
-    let type_prise1 = "TODO"
-    let debut1 = new Date("2021-12-13T05:05:04.977000")
-    let fin1 = new Date("2021-12-13T05:05:04.977000")
-    let date_mise_en_service1 = new Date("2021-12-13T05:05:04.977000")
-    let longitude1 = -0.502617
-    let latitude1 = 47.481716
-    let commune1 = "Beaune"
-    let code_postal1 = "49124"
+    for(let b of data.bornes){
+      let station = new Station(b.nom_station, b.adresse_station)
 
-    let station1 = new Station(nom_station1, adresse_station1)
-    let horaire1 = new Horaire(debut1, fin1)
-    let coordonnees1 = new Coordonnees(longitude1, latitude1)
-    let ville1 = new Ville(commune1, code_postal1)
+      let horaires: Horaire[] = []
+      horaires = [
+        new Horaire(new Date("2020-01-01 0:00:00"), new Date("2020-01-01 23:59:59"))
+      ]
 
-    bornes.push(new Borne(nom_enseigne1, station1, type_prise1, [horaire1], date_mise_en_service1, coordonnees1, ville1))
+      let coords = new Coordonnees(b.consolidated_longitude, b.consolidated_latitude)
+      let ville = new Ville(b.consolidated_commune, b.code_insee_commune.toString())
 
-
-    // Borne 2
-    let nom_enseigne2 = "ELECTRA"
-    let nom_station2 = "Beaune - Greet Hotel"
-    let adresse_station2 = "58 route de Verdun 21200 Beaune"
-    let type_prise2 = "TODO"
-    let debut2 = new Date("2021-12-13T05:05:04.977000")
-    let fin2 = new Date("2021-12-13T05:05:04.977000")
-    let date_mise_en_service2 = new Date("2022-03-30T05:05:04.977000")
-    let longitude2 = 4.851826
-    let latitude2 = 47.009538
-    let commune2 = "Beaune"
-    let code_postal2 = "21200"
-
-    let station2 = new Station(nom_station2, adresse_station2)
-    let horaire2 = new Horaire(debut2, fin2)
-    let coordonnees2 = new Coordonnees(longitude2, latitude2)
-    let ville2 = new Ville(commune2, code_postal2)
-
-    bornes.push(new Borne(nom_enseigne2, station2, type_prise2, [horaire2], date_mise_en_service2, coordonnees2, ville2))
-
-    // Borne 3
-    let nom_enseigne3 = "ELECTRA"
-    let nom_station3 = "Bordeaux - Novotel Bordeaux Lac"
-    let adresse_station3 = "rue Jean Samazeuilh 33300 Bordeaux"
-    let type_prise3 = "TODO"
-    let debut3 = new Date("2021-12-13T05:05:04.977000")
-    let fin3 = new Date("2021-12-13T05:05:04.977000")
-    let date_mise_en_service3 = new Date("2022-06-22T05:05:04.977000")
-    let longitude3 = -0.567111
-    let latitude3 = 44.890889
-    let commune3 = "Bordeaux"
-    let code_postal3 = "33300"
-
-    let station3 = new Station(nom_station3, adresse_station3)
-    let horaire3 = new Horaire(debut3, fin3)
-    let coordonnees3 = new Coordonnees(longitude3, latitude3)
-    let ville3 = new Ville(commune3, code_postal3)
-
-    bornes.push(new Borne(nom_enseigne3, station3, type_prise3, [horaire3], date_mise_en_service3, coordonnees3, ville3))
+      bornes.push(
+        new Borne(b.nom_enseigne, station, "Toutes", horaires, new Date(b.date_mise_en_service), coords, ville)
+      )
+    }
 
     return bornes
   }
