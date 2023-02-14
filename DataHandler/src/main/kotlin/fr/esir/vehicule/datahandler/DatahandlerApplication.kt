9ef9b934bb8d1b199530452613b2df2012d3252e
fgdou <fabigoardou@gmail.com>
2023-02-14 08:58:34 +0100

    fix: Bornes repos to datahandler

--- DataHandler/src/main/kotlin/fr/esir/vehicule/datahandler/DatahandlerApplication.kt
+++ DataHandler/src/main/kotlin/fr/esir/vehicule/datahandler/DatahandlerApplication.kt
@@ -1,9 +1,11 @@
 package fr.esir.vehicule.datahandler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EntityScan(basePackages = ["fr.esir.vehicules.dbobjects.bornes"])
 class DatahandlerApplication
 
 fun main(args: Array<String>) {
