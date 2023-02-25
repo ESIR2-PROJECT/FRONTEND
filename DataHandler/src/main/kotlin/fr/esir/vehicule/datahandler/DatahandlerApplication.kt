f6402c505e35277e10ee5c3715ec3dfac421023e
fgdou <fabigoardou@gmail.com>
2023-02-25 18:40:24 +0100

    feat: scheduler for datahandler

--- DataHandler/src/main/kotlin/fr/esir/vehicule/datahandler/DatahandlerApplication.kt
+++ DataHandler/src/main/kotlin/fr/esir/vehicule/datahandler/DatahandlerApplication.kt
@@ -1,20 +1,37 @@
 package fr.esir.vehicule.datahandler
 
+import fr.esir.vehicule.datahandler.service.BornesService
 import fr.esir.vehicule.datahandler.service.PrisesService
 import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
+@EnableScheduling
 @EntityScan(basePackages = ["fr.esir.vehicules.dbobjects.bornes"])
 class DatahandlerApplication(
-		val prisesService: PrisesService
+		val prisesService: PrisesService,
+		val bornesService: BornesService
 ) {
+
+	@Value("\${update.on.startup}")
+	val updateOnStartup = false
 
 	@PostConstruct
 	fun init(){
 		prisesService.checkPrises()
+
+		if(updateOnStartup)
+			everyDay()
+	}
+
+	@Scheduled(cron = "0 0 1 * * *")
+	fun everyDay(){
+		bornesService.updateBornes()
 	}
 }
 
--- DataHandler/src/main/kotlin/fr/esir/vehicule/datahandler/config/Config.kt
+++
@@ -1,8 +0,0 @@
-package fr.esir.vehicule.datahandler.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.EnableScheduling
-
-@Configuration
-@EnableScheduling
-class Config
