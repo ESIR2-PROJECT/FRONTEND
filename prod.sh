bd42ba08fe44bd732271c025ec8b8b07796930ff
fgdou <fabigoardou@gmail.com>
2023-03-02 09:39:13 +0100

    feat: prod build for db object

--- prod.sh
+++ prod.sh
@@ -1,1 +1,6 @@
+docker compose -f compose.prod.yml build dbobjects
+docker compose -f compose.prod.yml create dbobjects
+docker compose -f compose.prod.yml cp dbobjects:app/target/dbobjects-0.0.1-SNAPSHOT.jar ./DBobjects/out/
 docker compose -f compose.prod.yml build
+docker compose -f compose.prod.yml up
+rm -Rf ./DBobjects/out
