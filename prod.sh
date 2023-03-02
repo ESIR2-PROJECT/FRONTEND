b156d58ffa7f4b8481d8762b959c68c8b89ed4ea
fgdou <fabigoardou@gmail.com>
2023-03-02 14:26:41 +0100

    feat: api build prod

--- prod.sh
+++ prod.sh
@@ -1,6 +1,7 @@
+mkdir ./API/lib
 docker compose -f compose.prod.yml build dbobjects
 docker compose -f compose.prod.yml create dbobjects
-docker compose -f compose.prod.yml cp dbobjects:app/target/dbobjects-0.0.1-SNAPSHOT.jar ./DBobjects/out/
+docker compose -f compose.prod.yml cp dbobjects:app/target/dbobjects-0.0.1-SNAPSHOT.jar ./API/lib/dbobjects.jar
 docker compose -f compose.prod.yml build
 docker compose -f compose.prod.yml up
-rm -Rf ./DBobjects/out
+rm -Rf ./API/lib
