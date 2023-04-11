4c5b70171d6402a68e890267b99a2e1ae0449e97
fgdou <fabigoardou@gmail.com>
2023-04-11 15:39:40 +0200

    feat: repo

---
+++ API/src/main/kotlin/fr/esir/vehicules/api/objects/VoiturePoint.kt
@@ -0,0 +1,9 @@
+package fr.esir.vehicules.api.objects
+
+class VoiturePoint(
+    val codepostal: String,
+    val total: Int,
+    val electric: Int,
+    val gaz: Int
+) {
+}
--- API/src/main/kotlin/fr/esir/vehicules/api/repos/VoituresRepository.kt
+++ API/src/main/kotlin/fr/esir/vehicules/api/repos/VoituresRepository.kt
@@ -1,7 +1,14 @@
 package fr.esir.vehicules.api.repos
 
+import fr.esir.vehicules.api.objects.VoiturePoint
 import fr.esir.vehicules.dbobjects.voitures.Voitures
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface VoituresRepository: CrudRepository<Voitures, Int> {
+    @Query("SELECT new fr.esir.vehicules.api.objects.VoiturePoint(V1.codgeo, V2.nbVp, V2.nbVpRechargeablesEl, V2.nbVpRechargeablesGaz) FROM (" +
+            "SELECT V.codgeo, MAX(V.dateArrete) AS date FROM Voitures V GROUP BY V.codgeo" +
+            ") V1 " +
+            "JOIN Voitures V2 ON V1.codgeo = V2.codgeo AND V1.date = V2.dateArrete")
+    fun getAll(): List<VoiturePoint>
 }
