75fb2361d53c4818e5d3e2d078c92579ad6dc206
fgdou <fabigoardou@gmail.com>
2023-04-11 15:22:51 +0200

    drop voitures

--- API/src/main/kotlin/fr/esir/vehicules/api/repos/VoituresRepository.kt
+++
@@ -1,7 +0,0 @@
-package fr.esir.vehicules.api.repos
-
-import fr.esir.vehicules.dbobjects.voitures.Voitures
-import org.springframework.data.repository.CrudRepository
-
-interface VoituresRepository : CrudRepository<Voitures, Int> {
-}
