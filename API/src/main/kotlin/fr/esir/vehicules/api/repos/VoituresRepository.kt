63aeec786af50a8ae8bbc448f459de21bb0f66e7
fgdou <fabigoardou@gmail.com>
2023-04-11 17:05:42 +0200

    fix startup

--- API/src/main/kotlin/fr/esir/vehicules/api/repos/VoituresRepository.kt
+++ API/src/main/kotlin/fr/esir/vehicules/api/repos/VoituresRepository.kt
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface VoituresRepository: CrudRepository<Voitures, Int> {
-    @Query("SELECT new fr.esir.vehicules.api.objects.VoiturePoint(V1.codgeo, V2.nbVp, V2.nbVpRechargeablesEl, V2.nbVpRechargeablesGaz) FROM (" +
-            "SELECT V.codgeo, MAX(V.dateArrete) AS date FROM Voitures V GROUP BY V.codgeo" +
-            ") V1 " +
-            "JOIN Voitures V2 ON V1.codgeo = V2.codgeo AND V1.date = V2.dateArrete")
+    @Query("SELECT new fr.esir.vehicules.api.objects.VoiturePoint(V2.codgeo, V2.nbVp, V2.nbVpRechargeablesEl, V2.nbVpRechargeablesGaz) FROM (" +
+            "SELECT V.codgeo as postalcode, max(V.dateArrete) AS date FROM Voitures V GROUP BY V.codgeo" +
+            ") AS V1 " +
+            "JOIN Voitures V2 ON V1.postalcode = V2.codgeo AND V1.date = V2.dateArrete")
     fun getAllVille(): List<VoiturePoint>
     @Query("SELECT new fr.esir.vehicules.api.objects.VoitureAllPoint(V.codgeo, V.nbVp, V.nbVpRechargeablesEl, V.nbVpRechargeablesGaz, V.dateArrete) FROM Voitures V")
     fun getAll(): List<VoitureAllPoint>
